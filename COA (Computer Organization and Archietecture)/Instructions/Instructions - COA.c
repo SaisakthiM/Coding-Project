0ca40ba5384b1a24150b230bd4fd078c5b4a2643
Saisakthi <saisakthicloud@gmail.com>
2025-08-31 19:11:53 +0530

    Added New Folder COA

---
+++ COA (Computer Organization and Archietecture)/Instructions/Instructions - COA.c
@@ -0,0 +1,17 @@
+/*
+
+This is a Course by Neso Academy on theory side of Computer
+
+COA - Computer Organization and Architecture
+
+So what is COA 
+Take a building, if you want to build a building you need a proper plan and "Archietecture" first
+then "Organize" the people to buld it for you
+
+Similarly, if you want to build a Software, you have to "Architect" the features 
+and "Organize" the resources needed to buid that
+
+
+
+
+*/
