e038704856e11cc84fab83f86f69ed4f690e675e
Saisakthi <saisakthicloud@gmail.com>
2025-08-31 10:13:23 +0530

    Add folder: Go

---
+++ Go/Instructions - Go/Instructions.go
@@ -0,0 +1,15 @@
+package main
+
+/* 
+
+Go : It is a language which is not that famous but very useful.
+It is a machine compile language which converts its code into machine code and outputs it.
+
+There are 6 main points in GO language 
+
+1) it is statistically typed language. 
+Which means the variable 
+
+
+
+*/
---
+++ Go/Other Files/Main.go
@@ -0,0 +1,8 @@
+package main
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Println("")
+	fmt.Print("Hi, World")
+}
