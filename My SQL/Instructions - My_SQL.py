8fdb2947c0ecc9116746f6c1f72eee6d6bdf00bb
Saisakthi <saisakthicloud@gmail.com>
2025-08-31 10:13:28 +0530

    Add folder: My SQL

---
+++ My SQL/Instructions - My_SQL.py
@@ -0,0 +1,80 @@
+"""
+For Learning My SQL, We have to learn about databases.
+
+as internet grown to large amounts in 90's, we cannot store the data in normal format
+so we invented SQL or Structured Query Language. for storing data in database.
+
+(Note : To Run a Query in MySQL workbench, we need to use ctrl+enter for part under keyboard and ctrl+shift+enter for all part)
+
+Database : It is used to store data in a table and that table can be edited with SQL.
+
+there are different types of SQL and one of it is MY SQL.
+
+We also need ot know about different types of commands in SQL.
+
+
+Summary Table
+
+Command Type	Description	                        Common Commands
+DDL	            Define database structure	CREATE, ALTER, DROP, TRUNCATE
+DML	            Manipulate data	            INSERT, UPDATE, DELETE
+DQL	            Retrieve data	            SELECT
+DCL	            Control access	            GRANT, REVOKE
+TCL	            Manage transactions	        COMMIT, ROLLBACK, SAVEPOINT
+
+these are the basic commands we need to know about SQL.
+
+so we right out start with DDL commands.
+1) create database database_name : this is a syntax for creating a database in SQL.
+for eg : create database saisakthi
+
+2) drop database database_name : this is a syntax for deleting a database in SQL.
+for eg : drop database saisakthi
+
+3) create table table_name (column1 datatype, column2 datatype, ...); : this is a syntax for creating a table in SQL.
+for eg : create table student (name varchar(20), age int
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
