14d0f242570cdd1937968b55f35e23beff7a2ee3
Saisakthi <saisakthicloud@gmail.com>
2025-08-31 10:11:21 +0530

    Add folder: AI

---
+++ AI/Instructions/Instructions - AI.py
@@ -0,0 +1,51 @@
+"""
+
+This instruction is about learning AI with Python.
+
+First come to Syntax
+
+• Basic Arithmetic Operations
+• Control and conditional structures
+• Looping - For, while loops
+• User Input
+• Strings
+• Integers
+• Floating Values
+• Typecasting
+
+If you know it, just save it in a notebook 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
