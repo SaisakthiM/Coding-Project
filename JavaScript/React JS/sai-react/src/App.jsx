1edd56583ba1f9c74055c64a4956bdb316159fb4
Saisakthi <saisakthicloud@gmail.com>
2025-09-06 21:14:25 +0530

    Changes made on Date : 2025-09-06

--- JavaScript/React JS/sai-react/src/App.jsx
+++ JavaScript/React JS/sai-react/src/App.jsx
@@ -1,8 +1,18 @@
 import List from './List.jsx'
 function App() {
+	const fruits = [{name: "apple", calories: 22}, {name: "orange", calories: 82}, {name: "banana", calories: 19}, {name: "coconut", calories: 37}, {name: "pineapple", calories: 41}];
+	const vegetables = [
+  { name: "carrot", calories: 41 },
+  { name: "broccoli", calories: 55 },
+  { name: "spinach", calories: 23 },
+  { name: "potato", calories: 77 },
+  { name: "tomato", calories: 18 }
+];
+
 	return (
 		<>
-		<List></List>
+		<List items={fruits} category="fruits"></List>
+		<List items={vegetables} category="vegetables"></List>
 		</>
 	);
 }
