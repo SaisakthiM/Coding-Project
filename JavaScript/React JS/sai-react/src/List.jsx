34cc3e50e0505351e09e6a543717efc247546e6d
SaisakthiM <saisakthicloud@gmail.com>
2025-09-07 09:07:59 +0530

    Changes made on Date : 2025-09-07

--- JavaScript/React JS/sai-react/src/List.jsx
+++ JavaScript/React JS/sai-react/src/List.jsx
@@ -1,21 +1,36 @@
-function List({items, category}) {
-    const fruits = items;
+import PropTypes from "prop-types";
+function List({
+	items = [
+		{ name: "item1", calories: 10 },
+		{ name: "item2", calories: 20 },
+		{ name: "item3", calories: 30 }
+	],
+	category = "list_of_items"
+}) 
+	{
+	const fruits = [...items];
 
-	// fruits.sort((a,b) => a.name.localeCompare(b.name)); : Alphabetical Order
-	// fruits.sort((a,b) => b.name.localeCompare(a.name)); :  Reverse Alphabetical Order
-	// fruits.sort((a,b) => b.calories-a.calories); : Numerical 
-	// fruits.sort((a,b) => a.calories-b.calories); : Reverse Numerical 
-
-    const list_fruits = fruits.map((fruit) => (
-	<li key={fruit.name}>{fruit.name}:&nbsp; {fruit.calories}</li>
+	const list_fruits = fruits.map((fruit) => (
+		<li key={fruit.name}>
+		{fruit.name}: {fruit.calories}
+		</li>
 	));
-	
 
-    return (<>
-	<h3 className="list-category">{category}</h3>
-	<ul className="list-items">{list_fruits}</ul>
-	</>);
-    
+	return (
+		<>
+		<h3 className="list-category">{category}</h3>
+		<ul className="list-items">{list_fruits}</ul>
+		</>
+	);
 }
+List.propTypes = {
+	items: PropTypes.arrayOf(
+		PropTypes.shape({
+		name: PropTypes.string.isRequired,
+		calories: PropTypes.number.isRequired
+		})
+	),
+	category: PropTypes.string
+};
 
 export default List;
