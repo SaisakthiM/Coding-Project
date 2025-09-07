653d15ace38cdcbe3fc0ed785df87f38f782fbd2
SaisakthiM <saisakthicloud@gmail.com>
2025-09-07 12:44:32 +0530

    Changes made on Date : 2025-09-07

--- JavaScript/React JS/Instructions/intro-app/src/App.jsx
+++ JavaScript/React JS/Instructions/intro-app/src/App.jsx
@@ -5,6 +5,14 @@ import Card from './Card.jsx';
 import Button from './Buttons/Button.jsx';
 
 function App() {
+  const fruits = [{name: "apple", calories: 22}, {name: "orange", calories: 82}, {name: "banana", calories: 19}, {name: "coconut", calories: 37}, {name: "pineapple", calories: 41}];
+	const vegetables = [
+  { name: "carrot", calories: 41 },
+  { name: "broccoli", calories: 55 },
+  { name: "spinach", calories: 23 },
+  { name: "potato", calories: 77 },
+  { name: "tomato", calories: 18 }
+];
 	return (<div>
         <Header></Header>
         <Food></Food>
@@ -18,6 +26,9 @@ function App() {
         <Student /> {/* Uses default props */}
         <UserGreeeting isLoggedIn={true} username="Sai"></UserGreeeting>
 		<UserGreeeting></UserGreeeting>
+        <List items={fruits} category='Fruits'></List>
+		<List items={vegetables} category="vegetables"></List>
+		<List></List>
     </div>);
 }
 
