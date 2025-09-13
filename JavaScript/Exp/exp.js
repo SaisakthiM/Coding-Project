2762be3b3daf2f7e6f6f54cfe1bb0fc54cc84207
SaisakthiM <saisakthicloud@gmail.com>
2025-09-13 11:12:36 +0530

    Changes made on Date : 2025-09-13

--- JavaScript/Exp/exp.js
+++ JavaScript/Exp/exp.js
@@ -1,1 +1,24 @@
-console.log(new Date().getUTCFullYear());
+let arr = [1,2,3,4,5,6,7,8,2,5];
+
+function findDuplicate(arr) {
+    let hash = {};
+    let result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (hash[arr[i]]) {
+            hash[arr[i]] += 1;
+        } else {
+            hash[arr[i]] = 1;
+        }
+    }
+
+    // Collect only duplicates
+    for (let key in hash) {
+        if (hash[key] > 1) {
+            result.push(Number(key));
+        }
+    }
+
+    console.log("Duplicates:", result);
+}
+findDuplicate(arr);
--- JavaScript/React JS/sai-react/src/MyComponent.jsx
+++ JavaScript/React JS/sai-react/src/MyComponent.jsx
@@ -1,14 +1,21 @@
 import React, {useState} from 'react';
 function MyComponent() {
     const [name, setName] = useState("Sai");
+	const [age, setAge] = useState(0);
+
 
     setTimeout(() => {
         setName("Saisakthi")
     }, 2000)
 
+	
+	
+
 	return (
 		<div>
 			<p>name : {name}</p>
+			<p>age : {age}</p>
+
 		</div>
 	);
 }
