f4cb994f761282b16e476d50ee8240ad05697929
SaisakthiM <saisakthicloud@gmail.com>
2025-09-14 18:21:35 +0530

    Changes made on Date : 2025-09-14

--- JavaScript/React JS/sai-react/src/MyComponent.jsx
+++ JavaScript/React JS/sai-react/src/MyComponent.jsx
@@ -1,34 +1,12 @@
-import React, { useState } from 'react';
-
+import React, {useState} from "react";
 function MyComponent() {
-    const [name, setName] = useState("Sai");
-    const [age, setAge] = useState(0);
-    const [employed, setEmployed] = useState(false);
-    const [input, getInput] = useState('');
+    const [name, setname] = useState("Guest");
+    const handleChangeName = (event) => setname(event.target.value);
+    return (<div>
+        <input onChange={handleChangeName}></input>
+        <p>name : {name}</p>
+    </div>)
 
-    const incrementAge = () => setAge(age + 1);
-
-    return (
-        <div>
-            <p>Name: {name}</p>
-            <input 
-                type="text" 
-                placeholder="Enter your name" 
-                value={input} 
-                onChange={(e) => getInput(e.target.value)} 
-                className='nameInput'
-            />
-            <button className='input_box' onClick={() => setName(input)}>
-                Click to change the name
-            </button>
-
-            <p>Age: {age}</p>
-            <button onClick={incrementAge}>Increment Age</button>
-
-            <p>Employed: {employed.toString()}</p>
-            <button onClick={() => setEmployed(!employed)}>Change Employment Status</button>
-        </div>
-    );
 }
 
 export default MyComponent;
