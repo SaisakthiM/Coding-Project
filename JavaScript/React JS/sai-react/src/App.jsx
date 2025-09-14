28ffb550dbe1b440b0208e8fb671673e2b737ecc
SaisakthiM <saisakthicloud@gmail.com>
2025-09-14 18:21:35 +0530

    Changes made on Date : 2025-09-14

--- JavaScript/React JS/sai-react/src/App.jsx
+++ JavaScript/React JS/sai-react/src/App.jsx
@@ -1,10 +1,8 @@
-import MyComponent from './MyComponent.jsx';
-import Counter from './Counter.jsx';
-
+import MyComponent from "./MyComponent";
 function App() {
-	return (<Counter>
-		
-	</Counter>)
+	return (<MyComponent>
+
+	</MyComponent>);
 }
 
 export default App;
