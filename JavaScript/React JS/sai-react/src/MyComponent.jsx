ede0e5dce3cbefb2d3c305b871c5d718ace65beb
SaisakthiM <saisakthicloud@gmail.com>
2025-09-20 17:45:39 +0530

    Changes made on Date : 2025-09-20

--- JavaScript/React JS/sai-react/src/MyComponent.jsx
+++ JavaScript/React JS/sai-react/src/MyComponent.jsx
@@ -1,10 +1,14 @@
 import React, {useState} from "react";
+import { use } from "react";
 function MyComponent() {
     const [name, setname] = useState("Guest");
+    const [quantity, setQuantity] = useState(0);
     const handleChangeName = (event) => setname(event.target.value);
     return (<div>
         <input onChange={handleChangeName}></input>
         <p>name : {name}</p>
+        <input value={quantity} onChange={(e) => setQuantity(e.target.value)} type="number"></input>
+        <p>Qunatity : {quantity}</p>
     </div>)
 
 }
---
+++ Projects/Social Media App/microservice-go/go.mod
@@ -0,0 +1,3 @@
+module github.com/SaisakthiM/microservice-go
+
+go 1.25
