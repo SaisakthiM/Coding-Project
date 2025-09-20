919457c7a9971d6c489fa69986280fd80cee646d
SaisakthiM <saisakthicloud@gmail.com>
2025-09-20 20:06:19 +0530

    Changes made on Date : 2025-09-20

--- JavaScript/React JS/sai-react/src/App.jsx
+++ JavaScript/React JS/sai-react/src/App.jsx
@@ -1,8 +1,11 @@
 import MyComponent from "./MyComponent";
+import PackingList from "./PackingList";
 function App() {
-	return (<MyComponent>
-
-	</MyComponent>);
+	return (<>
+	<MyComponent>
+	</MyComponent>
+	<PackingList></PackingList>
+	</>);
 }
 
 export default App;
