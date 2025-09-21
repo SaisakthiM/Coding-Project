04a679f625947122cdfaff4d48027be3d279c512
SaisakthiM <saisakthicloud@gmail.com>
2025-09-21 09:39:49 +0530

    Changes made on Date : 2025-09-21

--- Projects/Social Media App/frontend/src/main.jsx
+++ Projects/Social Media App/frontend/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AppRouter from "./Router"; // <-- your router file
+import { VisionUIControllerProvider } from "./context";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <VisionUIControllerProvider>
+      <AppRouter />
+    </VisionUIControllerProvider>
+  </React.StrictMode>
+);
