c4775dbd2edc9ea83aa7f088d3580e56bd250e83
SaisakthiM <saisakthicloud@gmail.com>
2025-09-22 22:26:05 +0530

    Changes made on Date : 2025-09-22

--- Projects/Social Media App/microservice-java/src/test/java/sai_group/sai_java/SaiJavaApplicationTests.java
+++ Projects/Social Media App/microservice-java/src/test/java/sai_group/sai_java/SaiJavaApplicationTests.java
@@ -1,13 +1,20 @@
 package sai_group.sai_java;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import sai_group.sai_java.repository.UserRepository;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class SaiJavaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private UserRepository userRepository;
 
+    @Test
+    void contextLoads() {
+        assertNotNull(userRepository);
+    }
 }
