b419fa6720af81633dd12e60625e06b8405ada17
SaisakthiM <saisakthicloud@gmail.com>
2025-10-01 19:19:29 +0530

    Changes made on Date : 2025-10-01

---
+++ Projects/Quiz App/quiz-app/src/components/Quiz.jsx
@@ -0,0 +1,24 @@
+import './quiz.css'
+
+export default function Quiz() {
+    return (<>
+        <div className='contaier'>
+            <h1>Quiz App</h1>
+            <hr></hr> 
+            <h2>Which device is required for a internet connection ?</h2>
+            <ul>
+                <li>Modem</li>
+                <li>Router</li>
+                <li>LAN Cable</li>
+                <li>Pen Drive</li>
+            </ul>
+            <button>Next</button>
+            
+            <div className='index'>
+                <br></br>
+                <p>1 of 5 Questions</p>
+            </div>
+        </div>
+    </>)
+}
+
