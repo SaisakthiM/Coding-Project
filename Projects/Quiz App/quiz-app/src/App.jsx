5bdd27311bd9df84a6b62546b858b40bec78c29d
SaisakthiM <saisakthicloud@gmail.com>
2025-10-01 19:19:29 +0530

    Changes made on Date : 2025-10-01

---
+++ Projects/Quiz App/quiz-app/src/App.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Quiz from "./components/Quiz";
+
+
+export default function App() {
+	return (<>
+		<Quiz/>
+	</>)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
