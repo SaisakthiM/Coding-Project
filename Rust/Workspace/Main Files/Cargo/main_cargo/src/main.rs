ca95730347dc9635c56503db885da45274be812a
SaisakthiM <saisakthicloud@gmail.com>
2025-10-01 14:20:12 +0530

    Changes made on Date : 2025-10-01

---
+++ Rust/Workspace/Main Files/Cargo/main_cargo/src/main.rs
@@ -0,0 +1,10 @@
+use std::io;
+
+fn main() {
+    println!("Guess a number : ");
+    let mut guess = String::new();
+    let r2 = &mut guess;
+    println!("{r2}");
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    println!("You guessed: {guess}");
+}
