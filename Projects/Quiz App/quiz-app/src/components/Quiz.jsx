da00f9abeb67bde1e68875fc049b26a85513c5c0
SaisakthiM <saisakthicloud@gmail.com>
2025-10-02 08:47:14 +0530

    Changes made on Date : 2025-10-02

--- Projects/Quiz App/quiz-app/src/components/Quiz.jsx
+++ Projects/Quiz App/quiz-app/src/components/Quiz.jsx
@@ -1,20 +1,34 @@
-import './quiz.css'
+import './quiz.css';
+import { useState } from "react";
 
-export default function Quiz({Questions, Options, Answer}) {
+export default function Quiz({Questions, Options, Answer, Mark=false}) {
+    const [selected, setSelected] = useState(null); // store which option user clicked
+    const handleSelect = (index) => {
+        if (selected === null) { // allow only one choice
+            setSelected(index);
+        }
+    };
+    if (selected === Answer) {
+        Mark = true
+    }
+    else {
+        Mark = false
+    }
     return (<>
         <div className='container'>
             <h1>Quiz App</h1>
             <hr></hr> 
             <h2>1. {Questions}</h2>
             <ul>
-                <li>{Options[0]}</li>
-                <li>{Options[1]}</li>
-                <li>{Options[2]}</li>
-                <li>{Options[3]}</li>
-                
+                {Options.map(
+                    (option,index) => (
+                        <li key={index} onClick={() => handleSelect(index)} className={selected === index ? "selected" : ""}>{option}</li>
+                    )
+                )}
             </ul>
-            <button>Next</button>
-            
+            <div className="button_quiz">
+                <button>Next</button>
+            </div>
             <div className='index'>
                 <br></br>
                 <p>1 of 5 Questions</p>
