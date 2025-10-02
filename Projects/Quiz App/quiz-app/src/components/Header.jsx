488b7af4efa323186502d765e91969c27b82b360
SaisakthiM <saisakthicloud@gmail.com>
2025-10-02 11:01:12 +0530

    changes made on 2-10-2025

--- Projects/Quiz App/quiz-app/src/components/Header.jsx
+++ Projects/Quiz App/quiz-app/src/components/Header.jsx
@@ -1,7 +1,15 @@
-export default function Header() {
-	return (
-		<div className="container">
-					
-		</div>
-	)
+import './header.css'
+
+export default function Header({ onStart }) {
+  return (
+    <div className="container">
+      <h1>Quiz App</h1>
+      <hr /> 
+      <h3>This is a Quiz Game. You will be asked 5 questions about general CS topics.</h3>
+      <h2>Ready for the journey?</h2>
+      <div className='button_quiz'>
+        <button onClick={onStart}>Start</button>
+      </div>
+    </div>
+  )
 }
