6bc77c4256af459858d903b01e0eccbe9c038ce5
SaisakthiM <saisakthicloud@gmail.com>
2025-10-03 23:02:41 +0530

    Changes made on Date : 2025-10-03

--- Web Storm/Node JS/Instructions/Instructions - Node.js
+++ Web Storm/Node JS/Instructions/Instructions - Node.js
@@ -1,3 +1,32 @@
 /*
+Node JS : 
+
+Node.js is an open-source JavaScript runtime
+Uses the V8 JavaScript engine that Google Chrome uses
+Mostly used for developing server-side & networking apps/apis
+Takes JavaScript out of the browser
+Fast, scalable and popular in many areas of the industry
+
+There are some important thing in node js to learn 
+
+so the main thing is that Node js runs on a single thread
+a normal program uses multiple threads for concurency and
+
+there is that event loop 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 */
