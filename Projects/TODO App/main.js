4c0eb3e69ee21776257b6d089f37206281bedd79
SaisakthiM <saisakthicloud@gmail.com>
2025-10-05 17:25:49 +0530

    Changes made on Date : 2025-10-05

--- Projects/TODO App/main.js
+++ Projects/TODO App/main.js
@@ -3,6 +3,11 @@ window.addEventListener("DOMContentLoaded", () => {
     const input = document.getElementById("user_text");
     const note1 = document.getElementById("note1");
     const note2 = document.getElementById("note2");
+    const note3 = document.getElementById("note3");
+    const check1 = document.getElementById("check1");
+    const check2 = document.getElementById("check2");
+    const check3 = document.getElementById("check3");
+    
 
     button.addEventListener("click", function() {
         const text = input.value.trim();
@@ -12,10 +17,17 @@ window.addEventListener("DOMContentLoaded", () => {
             note1.textContent = text;
         } else if (!note2.textContent.length) {
             note2.textContent = text;
-        } else {
-            alert("Only 2 tasks allowed!");
+        } else if (!note3.textContent.length) {
+            note3.textContent = text;
+        }
+        else {
+            alert("Only 3 tasks allowed!");
         }
 
         input.value = "";
     });
+
+    if (check1.ariaChecked) {
+        alert("It is checked");
+    }
 });
