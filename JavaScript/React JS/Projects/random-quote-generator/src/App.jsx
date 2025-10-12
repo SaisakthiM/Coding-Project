cd83448baec2118c49e9b574681447103ae33d11
SaisakthiM <saisakthicloud@gmail.com>
2025-10-12 19:02:25 +0530

    Changes made on Date : 2025-10-12

--- JavaScript/React JS/Projects/random-quote-generator/src/App.jsx
+++ JavaScript/React JS/Projects/random-quote-generator/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 
 export default function App() {
-  return (
-    <div className="wrapper">
-      <div className="container">
-        
-      </div>
-    </div>
-  );
+	return (
+		<div className="wrapper">
+			<div className="container">
+				<nav>
+					<h1>Random Number Guesser</h1>
+				</nav>
+				<p>hi</p>
+			</div>
+		</div>
+	);
 }
