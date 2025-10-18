bea26ce09deef63ac7ec04ac28ea7cd018451b28
SaisakthiM <saisakthicloud@gmail.com>
2025-10-18 16:59:43 +0530

    Changes made on Date : 2025-10-18

--- JavaScript/React JS/Projects/todo-app/src/App.jsx
+++ JavaScript/React JS/Projects/todo-app/src/App.jsx
@@ -1,7 +1,15 @@
 import './App.css'
 
 function App() {
-	
+	return (
+	<div className='wrapper'>
+		<div className='container'>
+			<div>
+				hi
+			</div>
+		</div>
+	</div>
+)
 }
 
 export default App
---
+++ JavaScript/React JS/Projects/todo-app/src/components/ThemeProvider.jsx
@@ -0,0 +1,13 @@
+function ThemeProvider({ children }) {
+    
+    const [container, setContainer] = useState(`
+        .wrapper {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        .container {
+            
+        }
+        `)
+}
