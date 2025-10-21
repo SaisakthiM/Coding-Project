839f212aa6b331cb6ebc599a15724803373a4c98
SaisakthiM <saisakthicloud@gmail.com>
2025-10-21 16:53:48 +0530

    Changes made on Date : 2025-10-21

--- JavaScript/Node JS/Instructions/Instructions - Node.js
+++ JavaScript/Node JS/Instructions/Instructions - Node.js
@@ -1,25 +1,4 @@
 /*
-Node JS : 
-
-Node.js is an open-source JavaScript runtime
-Uses the V8 JavaScript engine that Google Chrome uses
-Mostly used for developing server-side & networking apps/apis
-Takes JavaScript out of the browser
-Fast, scalable and popular in many areas of the industry
-
-There are some important thing in node js to learn 
-
-so the main thing is that Node js runs on a single thread
-a normal program uses multiple threads for concurency and
-
-*/
-
-
-/* Importing the module in js : 
-
-There are 2 ways to import modules in Node JS
-
-1) 
 
 
 
