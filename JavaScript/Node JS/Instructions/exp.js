62806258f044b5157ddd4dc901ef43d34fbaef8d
SaisakthiM <saisakthicloud@gmail.com>
2025-10-23 18:25:28 +0530

    Changes made on Date : 2025-10-23

--- JavaScript/Node JS/Instructions/exp.js
+++ JavaScript/Node JS/Instructions/exp.js
@@ -1,4 +1,17 @@
-setTimeout(() => console.log("timeout"), 0);
-setImmediate(() => console.log("immediate"));
-process.nextTick(() => console.log("nextTick"));
-Promise.resolve().then(() => console.log("promise"));
+import axios from "axios";
+
+export default async function sendDataFromWeather(latitude, longitude) {
+    const url = "https://api.openmeteo.com/v1/forecast";
+    const params = {
+        latitude : latitude,
+        longitude : longitude
+    }
+    try {
+        const res = await axios.get(url, {params})
+        return res.data;
+    }
+    catch (err) {
+        return { error: err.message };
+    }
+
+}
