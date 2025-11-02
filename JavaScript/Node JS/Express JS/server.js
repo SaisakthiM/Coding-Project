5c3ee38014699239c4d4a5c3fefd8adfe9e9e746
SaisakthiM <saisakthicloud@gmail.com>
2025-11-02 10:50:48 +0530

    new express js

--- JavaScript/Node JS/Express JS/server.js
+++ JavaScript/Node JS/Express JS/server.js
@@ -1,13 +1,17 @@
 import express from "express";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
-let app = express();
+// Recreate __dirname manually (for ESM)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello world");
-})
+    res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.listen(8000, () => {
-    console.log("hi")
-})
-
+    console.log("Server running on http://localhost:8000");
+});
