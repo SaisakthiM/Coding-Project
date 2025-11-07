e51246bb68c2bbc34b5c5f445e45e52f8b611c12
SaisakthiM <saisakthicloud@gmail.com>
2025-11-07 17:28:51 +0530

    Changes made on Date : 2025-11-07

--- Projects/Notes App/backend/notes_app/api/views.py
+++ Projects/Notes App/backend/notes_app/api/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics, viewsets, permissions
 from django.contrib.auth.models import User
-from rest_framework import generics
-from .serializers import UserSerializer
-from rest_framework.permissions import IsAuthenticated, AllowAny
-
+from .serializers import UserSerializer, NoteSerializer
+from .models import Note
 
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [AllowAny]
+
+class NoteViewSet(viewsets.ModelViewSet):
+    serializer_class = NoteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):  # type: ignore[override]
+        return Note.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
--- Projects/Notes App/frontend/notes_app_frontend/src/components/RegistrationComplete.jsx
+++ Projects/Notes App/frontend/notes_app_frontend/src/components/RegistrationComplete.jsx
@@ -1,14 +1,20 @@
+import { useContext } from "react"
 import "../styles.css"
+import { UserProvider } from "./UserProvider"
+import { UserContext } from "./UserContext"
 
 export default function RegistrationComplete() {
-    
+    let {beforeLogin, setBeforeLogin} = useContext(UserContext)
+    let handleBeforeLogin = () => {
+        setBeforeLogin(true)
+    }
     return (
         <div className="wrapper">
             <div className="complete">
                     <div>
                         <h1>Registration Completed 🎉</h1>
                     	<h2>Go to Login Page</h2>
-                    	<button>Click Here</button>
+                    	<button onClick={handleBeforeLogin}>Click Here</button>
                     </div>
             </div>
         </div>
