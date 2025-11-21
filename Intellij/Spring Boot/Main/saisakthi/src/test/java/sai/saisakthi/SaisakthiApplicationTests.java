9c9e60b26ba3be3766363ed323e6e12b26bc5b6b
SaisakthiM <saisakthicloud@gmail.com>
2025-11-21 18:34:23 +0530

    Changes made on Date : 2025-11-21

---
+++ Intellij/Spring Boot/Main/saisakthi/src/test/java/sai/saisakthi/SaisakthiApplicationTests.java
@@ -0,0 +1,13 @@
+package sai.saisakthi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaisakthiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
