5cc9a0acc38ae1459cb24b23037f424edc735613
SaisakthiM <saisakthicloud@gmail.com>
2025-11-22 16:16:18 +0530

    Created Java Main

---
+++ Intellij/Main/Variable/Exercise/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        /* 1st task print hello and name in separate places */
+        System.out.println("Hello");
+        System.out.println("Saisakthi");
+    }
+}
