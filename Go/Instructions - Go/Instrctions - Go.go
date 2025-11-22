a5cba1201fe1737653490bfb5e4121fbd2a3e5dc
SaisakthiM <saisakthicloud@gmail.com>
2025-11-22 21:21:05 +0530

    Changes made on Date : 2025-11-22

---
+++ Go/Instructions - Go/Instrctions - Go.go
@@ -0,0 +1,29 @@
+package main
+/* 
+
+Six main Points about Go :
+1) Strongly Typed
+2) Statically typed
+3) Compiled language
+4) Fast compilation time
+5) Built in fast concurrency
+6) Simplicity
+
+package main
+import "fmt"
+
+func main() {
+	var name string;
+	fmt.Print("Enter your name: ");
+	fmt.Scan(&name);
+	fmt.Print(name);
+}
+
+this is a basic code
+1) package main : it is a package command which tells go where to search for the package 
+here it is main so it searches main one and use the packages
+2) import "fmt" : it is a package used mainl in go for printing and scanning 
+
+
+
+*/
