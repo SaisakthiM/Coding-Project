695c24d6095727496cf41287b7089002792e442c
SaisakthiM <saisakthicloud@gmail.com>
2025-11-23 11:58:35 +0530

    Changes made on Date : 2025-11-23

--- Intellij/Projects/Task Manager/task-manager/src/main/java/sai/saisakthi/models/Storage.java
+++ Intellij/Projects/Task Manager/task-manager/src/main/java/sai/saisakthi/models/Storage.java
@@ -1,5 +1,25 @@
 package sai.saisakthi.models;
 
+import java.util.ArrayList;
 public class Storage {
-    
+    static class Task {
+        int id;
+        String name;
+        boolean complete;
+        Task(int id, String name, boolean complete) {
+            this.id = id;
+            this.name = name;
+            this.complete = complete;
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Task>> task = new ArrayList<>();
+        
+        ArrayList<Task> task1 = new ArrayList<>();
+
+        task1.add(new Task(1, "main", false));
+        System.out.print(task1);
+    }
+
 }
