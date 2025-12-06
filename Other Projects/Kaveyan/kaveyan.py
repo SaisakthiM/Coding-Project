30dc61fda6ec2326ca10d5e485d7b826d97f0438
SaisakthiM <saisakthicloud@gmail.com>
2025-12-06 18:11:45 +0530

    Changes made on Date : 2025-12-06

--- Other Projects/Kaveyan/kaveyan.py
+++
@@ -1,9 +0,0 @@
-capitals = {"usa" : "washinton",
-        "india" : "new delhi",
-        "japan" : "tokoyo"}
-#print(capitals.get("canada"))
-capitals.update({"Germany" : "berlin"})
-#capitals.update({"usa" : "chennai"})
-#capitals.pop("usa")
-keys = capitals.keys()
-print(keys)
