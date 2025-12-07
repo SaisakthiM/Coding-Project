201bf3c46974077af4d02387204874edfead77c4
SaisakthiM <saisakthicloud@gmail.com>
2025-12-07 10:15:40 +0530

    Created Spring Boot

---
+++ Programming Languages/Intellij Java/Spring Boot/Main/saispring/src/main/java/com/springboot/saispring/SaispringApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.saispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaispringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaispringApplication.class, args);
+	}
+
+}
---
+++ Programming Languages/Intellij Java/Spring Boot/Main/saispring/src/test/java/com/springboot/saispring/SaispringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.saispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaispringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
