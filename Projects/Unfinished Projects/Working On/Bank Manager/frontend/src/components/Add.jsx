7081f077a17f0e3620087db2818a2ad3c541c17c
SaisakthiM <saisakthicloud@gmail.com>
2025-12-21 07:59:47 +0530

    Java Changes

--- Projects/Unfinished Projects/Working On/Bank Manager/frontend/src/components/Add.jsx
+++
@@ -1,7 +0,0 @@
-function Add() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
--- Projects/Unfinished Projects/Working On/Bank Manager/frontend/src/components/Welcome.jsx
+++ Projects/Unfinished Projects/Working On/Bank Manager/frontend/src/components/Welcome.jsx
@@ -1,19 +1,22 @@
 import { useNavigate } from "react-router-dom";
+
 export default function Welcome() {
     const navigate = useNavigate();
+    
     return (
         <div className="wrapper">
             <div className="container">
                 <h1>Welcome to Bank Manager Application</h1>
                 <h3>Here in the bank you can do the following things</h3>
                 <ol>
-                    <li><button onClick={() => {navigate('/add')}}>Add a Deposit</button></li>
-                    <li><button onClick={() => {navigate('/withdraw')}}>Withdraw from Deposit</button></li>
-                    <li><button onClick={() => {navigate('/loan')}}>Take a Loan</button></li>
-                    <li><button onClick={() => {navigate('/repay')}}>Repay Credit/Loan</button></li>
-                <h1>Enjoy your Experience</h1>
+                    <li><button onClick={() => navigate('/add')}>Add a Deposit</button></li>
+                    <li><button onClick={() => navigate('/withdraw')}>Withdraw from Deposit</button></li>
+                    <li><button onClick={() => navigate('/account')}>Get Account Details</button></li>
+                    <li><button onClick={() => navigate('/loan')}>Take a Loan</button></li>
+                    <li><button onClick={() => navigate('/repay')}>Repay Credit/Loan</button></li>
                 </ol>
+                <h1>Enjoy your Experience</h1>
             </div>
         </div>
-    )
+    );
 }
