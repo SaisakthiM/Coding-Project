f949e4ff20b376c280ddd81f787f89ec3c8a28e0
SaisakthiM <saisakthicloud@gmail.com>
2026-01-02 19:53:54 +0530

    new changes

---
+++ Programming Languages/Intellij Java/Spring Boot/Main/saihibernate/src/main/java/com/saihibernate/saihibernate/model/Student.java
@@ -0,0 +1,20 @@
+package com.saihibernate.saihibernate.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    @Id
+    private int rno;
+    private String name;
+    private String gender;
+    private String technology;
+
+}
