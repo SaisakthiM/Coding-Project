291f0a9042420a38f90ad20816edad53f12e1103
SaisakthiM <saisakthicloud@gmail.com>
2026-01-04 09:49:47 +0530

    new changes

--- Programming Languages/CSS/Tailwind CSS/Main/my-portfolio/src/components/Project.jsx
+++ Programming Languages/CSS/Tailwind CSS/Main/my-portfolio/src/components/Project.jsx
@@ -1,7 +1,22 @@
 export default function Projects() {
   return (
-    <div className="bg-[radial-gradient(at_50%_25%,rgb(0,121,4),rgba(0,0,0,1)_75%)] min-h-screen">
-      <div className="">
+    <div className="bg-[radial-gradient(at_50%_25%,rgb(0,121,4),rgba(0,0,0,1)_75%)] min-h-screen text-white">
+       <nav className="pt-5"> 
+        <h1 className="text-5xl flex justify-center pb-5">Projects</h1>
+      </nav>
+      <hr className="pb-5"></hr>
+      <div className="grid grid-cols-[1fr_auto_1fr] h-screen">
+
+  <div className="p-10">Left</div>
+
+  {/* Separator */}
+  <div className="w-px bg-white/40"></div>
+
+  <div className="p-10">Right</div>
+
+</div>
+
+
       </div>
       )
 }
