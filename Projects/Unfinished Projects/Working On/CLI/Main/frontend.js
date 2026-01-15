47e198b2c30527365576808f297dfef35ffbaf33
SaisakthiM <saisakthicloud@gmail.com>
2026-01-15 14:58:04 +0530

    main ne chanegs

--- Projects/Unfinished Projects/Working On/CLI/Main/frontend.js
+++ Projects/Unfinished Projects/Working On/CLI/Main/frontend.js
@@ -1,7 +1,7 @@
 import chalk from 'chalk';
 import inquirer from 'inquirer';
 import chalkAnimation from 'chalk-animation';
-import { exec } from 'child_process';
+import { exec, spawn } from 'child_process';
 import { promisify } from 'util';
 import { createSpinner } from 'nanospinner';
 import qs from 'qs';
@@ -11,29 +11,13 @@ import fs from 'fs';
 const execAsync = promisify(exec);
 const sleep = (ms = 800) => new Promise(r => setTimeout(r, ms));
 
-function runViteInit() {
-    return new Promise((resolve, reject) => {
-        const child = spawn(
-              'npm',
-                    ['create', 'vite@latest'],
-                          {
-                                  stdio: 'inherit', // <-- critical
-                                          shell: true
-                                                }
-                                                    );
-
-                                                        child.on('close', code => {
-                                                              if (code === 0) resolve();
-                                                                    else reject(new Error(`Vite exited with code ${code}`));
-                                                                        });
-                                                                          });
-}
-
 async function reactInitizer() {
   try {
-  const Spinner = createSpinner('Checking Node JS Version')
-  const {res} = await execAsync("node -v")
-  Spinner.success("Node JS Exists, Starting Vite Initializer")
+    const Spinner = createSpinner('Checking Node JS Version')
+    await execAsync("node -v")
+    Spinner.success("Node JS Exists, Starting Vite Initializer")
+    await spawn("npm create vite@latest") 
+    Spinner.success("Frontend Initialized");
 
   }
   catch (err) {
