71434d4641302c27b3b09a8fe2457947afe61f48
SaisakthiM <saisakthicloud@gmail.com>
2026-01-31 19:54:34 +0530

    mew name changes

--- Programming Languages/Clion C Family/C/Main/main.c
+++ Programming Languages/Clion C Family/C/Main/main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-enum los {
-  
-}
 int main() {
-  int* ptr = malloc(sizeof(int));
-  *ptr = 42;
-  free(ptr);
-  free(ptr);
-  return 0;
+  int fhar, celsius, step;
+  int lower = 0;
+  int upper = 300;
+  step = 20;
+  fhar = lower;
+  while (fhar <= upper) {
+    celsius = 5 * (fhar - 32) / 9;
+    printf("%d\t%d\n", fhar, celsius);
+    fhar = fhar + step;
+  }
+  double x = 5.0 / 9.0;
+  printf("%f", x);
 }
