3708eedad12a6a82775f5a11b13e7e191ec74af4
SaisakthiM <saisakthicloud@gmail.com>
2026-02-05 14:38:56 +0530

    new changes man

--- Programming Languages/Clion C Family/C/Main/main.c
+++ Programming Languages/Clion C Family/C/Main/main.c
@@ -1,16 +1,4 @@
+#include <inttypes.h>
 #include <stdio.h>
 
-int main() {
-  int fhar, celsius, step;
-  int lower = 0;
-  int upper = 300;
-  step = 20;
-  fhar = lower;
-  while (fhar <= upper) {
-    celsius = 5 * (fhar - 32) / 9;
-    printf("%d\t%d\n", fhar, celsius);
-    fhar = fhar + step;
-  }
-  double x = 5.0 / 9.0;
-  printf("%f", x);
-}
+int main() { printf("%lu", sizeof(long)); }
