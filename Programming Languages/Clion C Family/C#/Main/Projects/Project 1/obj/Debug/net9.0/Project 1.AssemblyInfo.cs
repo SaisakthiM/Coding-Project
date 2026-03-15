bc65fd856a4127ee623284312546555a02a727ad
SaisakthiM <saisakthicloud@gmail.com>
2026-03-15 19:49:07 +0530

    Main Changes

--- Programming Languages/Clion C Family/C#/Main/Projects/Project 1/obj/Debug/net9.0/Project 1.AssemblyInfo.cs
+++ Programming Languages/Clion C Family/C#/Main/Projects/Project 1/obj/Debug/net9.0/Project 1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cc6236e01eb0869d99c2bb2e62bfd630f0ff7dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c02c7ad1026c81b0b93175e3ae92a4a776ee597e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Programming Languages/Intellij Java/Instructions/Project/sai/src/main/java/com/generics/sai/SaiApplication.java
+++ Programming Languages/Intellij Java/Instructions/Project/sai/src/main/java/com/generics/sai/SaiApplication.java
@@ -1,13 +1,23 @@
 package com.generics.sai;
 
+import com.sai.generics.Car;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @SpringBootApplication
-public class SaiApplication {
+public class SaiApplication extends Car {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SaiApplication.class, args);
+		Car int1 = new Car();
+		List<Integer> integers = new ArrayList<>();
+		integers.add(1);
+		integers.add(1);
+		integers.add(1);
+		System.out.println(int1.ToCar(integers));
 	}
 
 }
