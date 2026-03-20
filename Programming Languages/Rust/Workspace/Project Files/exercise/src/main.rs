ce8f336aec7d843d9510cd6bd6a43c3556258142
SaisakthiM <saisakthicloud@gmail.com>
2026-03-20 11:19:34 +0530

    new changes

---
+++ Programming Languages/Rust/Workspace/Project Files/exercise/src/main.rs
@@ -0,0 +1,92 @@
+#![allow(unused)]
+#![allow(non_snake_case)]
+
+use rand;
+
+use std::io::{Read, Write, stdin, stdout};
+fn add_2() {
+    let mut x : String = String::from("");
+    let mut y : String = String::from("");
+
+    println!("Enter value 1 : ");
+    stdin().read_line(&mut x).expect("Error : cannot recieve input");
+
+    println!("Enter value 2 : ");
+    stdin().read_line(&mut y).expect("Error : cannot recieve input");
+
+    let fin1: i32 = x.trim().parse().expect("wrong input");
+    let fin2: i32 = y.trim().parse().expect("wrong input");
+
+    println!("{}", fin1+fin2);
+}
+
+fn int_to_str(a: i32) -> String{
+    let x : String = i32::to_string(&a);
+    return x;
+}
+
+enum Calc {
+    Addition, 
+    Subtraction,
+    Multiplication, 
+    Division
+}
+
+fn calculator(a: i64, b: i64, operation: Calc) -> i64{
+    match operation {
+        Calc::Addition => a+b,
+        Calc::Subtraction => a-b,
+        Calc::Multiplication => a*b,
+        Calc::Division => a/b,
+    }
+}
+
+fn is_odd_even(a: u32) -> String{
+    if a % 2 == 0 {
+        String::from("It is Even number")
+    }
+    else {
+        String::from("It is Odd number")
+    }
+}
+
+fn loops(a: u32) {
+    for i in (1..a).step_by(2) {
+        println!("{}", i);
+    }
+}
+
+fn calculate_age(DOB : i32) -> i32 {
+    let CURRENT_YEAR = 2026;
+    return CURRENT_YEAR - DOB;
+}
+
+fn calculate_area() -> f64{
+    let mut diameter: String = String::from("");
+    println!("Enter an diameter : ");
+    stdin().read_line(&mut diameter).expect("Error cannot get input");
+    let val: f64 = diameter.trim().parse().expect("error might happen");
+    let area: f64 = (3.14 * val * val)/4.0;
+    return area;
+
+}
+
+fn is_prime(val: u64) -> bool{
+    for i in 2..=(val as f64).sqrt() as u64 {
+        if val % i == 0 {
+            return false;
+        }
+    }
+    return true;
+}
+
+fn get_input() {
+    let mut input = String::from("");
+    println!("Enter a number : ");
+    stdin().read_line(&mut input).expect("Error cannot read the line");
+    
+}
+
+fn main() {
+    
+}
