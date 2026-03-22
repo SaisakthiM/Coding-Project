418613005bcbeaf7873c58cf663a3e452887d879
SaisakthiM <saisakthicloud@gmail.com>
2026-03-22 11:06:36 +0530

    nre chnanges

---
+++ Programming Languages/Intellij Java/Instructions/Project/new-proj/sai/src/main/java/com/new_proj/Main.java
@@ -0,0 +1,13 @@
+package com.new_proj;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collection;
+public class Main {
+    public static void main(String[] args) {
+        int a = 128;
+    Integer x = a;
+    Integer y = a;
+
+    System.out.println(x == y);
+    }
+}
