2f9d13e4917c6a1105c23ab6ee8fdb2c0a5cdd1a
SaisakthiM <saisakthicloud@gmail.com>
2026-03-31 19:21:51 +0530

    Changes made on Date : 2026-03-31

--- Programming Languages/JavaScript/Typescript/Instructions/Instructions - TS.ts
+++ Programming Languages/JavaScript/Typescript/Instructions/Instructions - TS.ts
@@ -1,24 +1,6 @@
 /*
 
-Ok we are learning TS here
-so what is Typescript 
 
-It is a superset of JS which compiles to JS 
-so remember, all JS code (Mostly) is valid TS code 
-
-so we are learning for one main thing and that's the type safety
-
-everyone including me is despice of JS loose typing 
-everyone hates it. so what did the Microsoft 
-they invented the TS a superset stricty typed language which compiles down to JS
-
-now most procution company uses TS 
-
-so that's intro
-there is not much new things to learn if you learnt JS basics and also any low level language like C 
-you are implementing the safe practise here in TS that's it 
-
-let's start with what we are learning 
 
 
 */
