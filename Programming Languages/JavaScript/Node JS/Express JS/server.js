a2aae326e0f0a20b759a1c551ca5c22083e4d413
SaisakthiM <saisakthicloud@gmail.com>
2026-04-02 20:18:24 +0530

    Changes made on Date : 2026-04-02

--- Programming Languages/JavaScript/Node JS/Express JS/server.js
+++ Programming Languages/JavaScript/Node JS/Express JS/server.js
@@ -1,33 +1,12 @@
-import express from "express";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
+import "express"
+import express from "express"
 
-// Recreate __dirname manually (for ESM)
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const app = express()
 
-const app = express();
+app.get('/main', (req, res) => {console.log("Hello")})
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
-
-app.get('/ab+cd', (req, res, next) => {
-  console.log("First pattern matched");
-  next(); // passes to next matching route
-});
-
-app.get('/ab*cd', (req, res) => {
-  res.send("Hi hello");
-});
-
-app.get('/ab*cd', (req,res) => {
-    res.send("Hi hello")
+app.listen(3000, () => {
+  console.log(`Example app listening on port ${port}`)
 })
 
-
-
-app.listen(8000, () => {
-    console.log("Server running on http://localhost:8000");
-});
-
+asdkasn  
