e90295ffd9739d4672dc769ebdea2fd3c8815d72
SaisakthiM <saisakthicloud@gmail.com>
2026-04-03 11:53:31 +0530

    Changes made on Date : 2026-04-03

--- Programming Languages/JavaScript/Node JS/Express JS/server.js
+++ Programming Languages/JavaScript/Node JS/Express JS/server.js
@@ -3,7 +3,13 @@ import express from "express"
 
 const app = express()
 
-app.get('/main', (req, res) => {res.send("hello")})
+app.get('/main',(req, res) => {res.send("hello")})
+
+app.post('/main',(req, res) => {res.send("hello")})
+
+app.put('/main',(req, res) => {res.send("hello")})
+
+app.delete('/main',(req, res) => {res.send("hello")})
 
 app.listen(3000, () => {
 })
