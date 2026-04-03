800238fcb27dd1075ab4ca05dad6a8bff19ea58f
SaisakthiM <saisakthicloud@gmail.com>
2026-04-03 08:23:39 +0530

    Changes made on Date : 2026-04-03

--- Programming Languages/JavaScript/Node JS/Express JS/server.js
+++ Programming Languages/JavaScript/Node JS/Express JS/server.js
@@ -3,10 +3,8 @@ import express from "express"
 
 const app = express()
 
-app.get('/main', (req, res) => {console.log("Hello")})
+app.get('/main', (req, res) => {res.send("hello")})
 
 app.listen(3000, () => {
-  console.log(`Example app listening on port ${port}`)
 })
 
-asdkasn  
