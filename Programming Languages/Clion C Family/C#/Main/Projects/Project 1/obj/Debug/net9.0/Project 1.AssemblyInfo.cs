5b425ca7ae38f0ff7734fd73b70ef79017126801
SaisakthiM <saisakthicloud@gmail.com>
2026-05-31 16:32:34 +0530

    Changes made on Date : 2026-05-31

--- Programming Languages/Clion C Family/C#/Main/Projects/Project 1/obj/Debug/net9.0/Project 1.AssemblyInfo.cs
+++ Programming Languages/Clion C Family/C#/Main/Projects/Project 1/obj/Debug/net9.0/Project 1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae2918332ebe647b33e340c730a355550eadc1c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37ee127fb4b2868dee41111487a1862affe98bc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Projects/Finished Projects/Docker/Terraform/projects/Social Media App/apps/frontend/vite.config.js
+++ Projects/Finished Projects/Docker/Terraform/projects/Social Media App/apps/frontend/vite.config.js
@@ -22,5 +22,10 @@ export default defineConfig({
     outDir: 'dist',
     sourcemap: false,
   },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./src/setupTests.js'],
+  }
 });
 
