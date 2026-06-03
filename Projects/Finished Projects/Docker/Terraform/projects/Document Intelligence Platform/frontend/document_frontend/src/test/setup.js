488edae109d43f5e6f20fa7d61bed61415ec7b52
SaisakthiM <saisakthicloud@gmail.com>
2026-06-03 11:11:38 +0530

    changes

--- Projects/Finished Projects/Docker/Terraform/projects/Document Intelligence Platform/frontend/document_frontend/src/test/setup.js
+++ Projects/Finished Projects/Docker/Terraform/projects/Document Intelligence Platform/frontend/document_frontend/src/test/setup.js
@@ -2,3 +2,6 @@
 import '@testing-library/jest-dom';
 import React from 'react';
 globalThis.React = React;
+
+// jsdom does not implement scrollIntoView
+window.HTMLElement.prototype.scrollIntoView = vi.fn();
