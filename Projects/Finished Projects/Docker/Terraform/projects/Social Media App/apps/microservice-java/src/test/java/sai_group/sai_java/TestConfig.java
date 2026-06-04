01e51d0dbe6aded3aece03dead0bf827743f18a8
SaisakthiM <saisakthicloud@gmail.com>
2026-06-04 09:17:46 +0530

    new change

--- Projects/Finished Projects/Docker/Terraform/projects/Social Media App/apps/microservice-java/src/test/java/sai_group/sai_java/TestConfig.java
+++ Projects/Finished Projects/Docker/Terraform/projects/Social Media App/apps/microservice-java/src/test/java/sai_group/sai_java/TestConfig.java
@@ -1,6 +1,7 @@
 package sai_group.sai_java;
 
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Configuration; // Change the import
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.cassandra.core.ReactiveCassandraTemplate;
 
@@ -12,7 +13,7 @@ import static org.mockito.Mockito.mock;
 
 import org.mockito.Mockito;
 
-@TestConfiguration
+@Configuration // <-- Change this from @TestConfiguration
 public class TestConfig {
     @Bean public NotificationRepository notificationRepository() { return mock(NotificationRepository.class); }
     @Bean public ActivityFeedRepository activityFeedRepository() { return mock(ActivityFeedRepository.class); }
