860da88baf1a146b35016af04cc29fb28e44fc38
SaisakthiM <saisakthicloud@gmail.com>
2026-06-21 11:56:29 +0530

    Changes made on Date : 2026-06-21

---
+++ Projects/Finished Projects/Docker/Terraform/infra/environments/prod-docker/.terragrunt-cache/hhg5ia_0Gk-7pftzNDS8rp697bs/sbLohQAPyqPbKpL3fFHjHKJDBts/environments/prod-docker/imports.tf
@@ -0,0 +1,99 @@
+import {
+  to = docker_container.compiler_db
+  id = "eb5bd32b580803714195d318357ea28005e3d651d7c13a5a36e413f325e3e880"
+}
+
+import {
+  to = docker_container.video_frontend_build
+  id = "67803977eed3bc60342edabe36be2ca1cae313b23fbd1372065f89a2c3b1da24"
+}
+
+import {
+  to = docker_container.blog_db
+  id = "307d32942e839336be4197f625c08f3c668f484020624e55bba420fd8ba4ca8d"
+}
+
+import {
+  to = docker_container.blog_minio
+  id = "9b1ae9f16da5524db9a66c70de0c1b530a339d11a7b4afe389b9a7d52a1565fd"
+}
+
+import {
+  to = docker_container.api_service_frontend_build
+  id = "5fb5d0614c9b3a03b85ff37b175dbbf22d1dcb6e0d3caecb7917a48518c194a2"
+}
+
+import {
+  to = docker_container.doc_mysql
+  id = "b54cfcdf8958848c28265317123e8fd8ce4b8a2fd8ff6754cf9a2ae40498e54f"
+}
+
+import {
+  to = docker_container.doc_minio
+  id = "a8c16a7242dcbe02678a775901153c69c6375c5b7ec3a2462695e844d71db833"
+}
+
+import {
+  to = docker_container.doc_frontend_build
+  id = "ea7c12602c06e5f4aca59de92ff12a29e221dbbbfa89a9ee046fdb98da2c295d"
+}
+
+import {
+  to = module.api_service_backend.docker_container.app
+  id = "e357839ae114b8e25b228675969ddb0675a23d7b3a7d79035866e0dd1c917fc1"
+}
+
+import {
+  to = module.bank_backend.docker_container.app
+  id = "382099f4250e29b543006205bd787c3dc5e8aaeafa9d821ac9e152311d599dd0"
+}
+
+import {
+  to = module.video_backend.docker_container.app
+  id = "54bbcc194e3a44384a88c9ab0e1a9930fefd830e35b8dd955a15e9e6616a516a"
+}
+
+import {
+  to = module.notes_backend.docker_container.app
+  id = "12eb1523e41f69daa2f21f4f5d692bb4e9e0cd0e96f14b2ee593eff4d920aa85"
+}
+
+import {
+  to = module.doc_backend.docker_container.app
+  id = "ee2d1c02c182b9e33086d58da811ba52c6486ef236eaaab43539271f95e2cb1a"
+}
+
+import {
+  to = module.hospital_management.docker_container.app
+  id = "ce2113ae058be2282ce5418b6630c31df1611700fcbe4e15f4818becefd63d96"
+}
+
+import {
+  to = docker_container.notes_postgres
+  id = "8ab88a8d9ac1c3832be6c307cf3130988f3ce6152e89c619eb638ba496e494f5"
+}
+
+import {
+  to = docker_container.whisper-postgres
+  id = "5b9fc0c70d3daee8e3967d15b9c60edb764d1c8423a5b09ea44c61381ca67cc1"
+}
+
+import {
+  to = docker_container.whisper_minio
+  id = "a8a24cccf7bfabfcc0105901d8830a6ebaabeb467a19b3eefdb3426556ac1a0f"
+}
+
+import {
+  to = docker_container.bank_postgres
+  id = "6d4c7be711ddb6274c1347d3414686f936aa55d5b09f39f0ec72cc9650351d69"
+}
+
+import {
+  to = module.blog_website.docker_container.app
+  id = "e23ed924b16dc3b641976d9ef98b4b74a7ed5eed0e8be5c62cd0a844dddab257"
+}
+
+import {
+  to = module.compiler_server.docker_container.app
+  id = "67489a7235f2f70151da08818e7eae7ad83726ffc12c5cbc64cf7321c16333f1"
+}
