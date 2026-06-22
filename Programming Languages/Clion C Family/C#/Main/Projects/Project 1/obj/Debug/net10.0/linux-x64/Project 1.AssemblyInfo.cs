9889a96bdcba3d99d11dc6114facc8862b307641
SaisakthiM <saisakthicloud@gmail.com>
2026-06-22 15:02:40 +0530

    changes

--- Programming Languages/Clion C Family/C#/Main/Projects/Project 1/obj/Debug/net10.0/linux-x64/Project 1.AssemblyInfo.cs
+++ Programming Languages/Clion C Family/C#/Main/Projects/Project 1/obj/Debug/net10.0/linux-x64/Project 1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2877d4c080777d4db66d0c19a84d4d10ddd296e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+861255c0f9f7b4c07f47a048e721fedf134395e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
