8a1f3d6bf9c40f3358f2eed22685e9fb140f3394
SaisakthiM <saisakthicloud@gmail.com>
2026-07-15 14:09:42 +0530

    changes

--- Programming Languages/Clion C Family/C#/Main/Projects/Project 1/obj/Debug/net10.0/linux-x64/Project 1.AssemblyInfo.cs
+++ Programming Languages/Clion C Family/C#/Main/Projects/Project 1/obj/Debug/net10.0/linux-x64/Project 1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d6e9928c90f5532bb61f28b5941c307432199dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5a535d8f4e17d43e66e3c2aa0171453f3f4512d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Programming Languages/Clion C Family/C/C Files/exercises/find_square.c
+++ Programming Languages/Clion C Family/C/C Files/exercises/find_square.c
@@ -1,5 +1,9 @@
+#include <cstdlib>
+#include <inttypes.h>
 #include <math.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int find() {
@@ -34,7 +38,150 @@ int sum_of_digits() {
 
 }
 
+int division(int numerator, int denominator,int *dividend, int *_remainder) {
+    if (denominator == 0) {
+        return 0;
+    }
+    *dividend = numerator / denominator;
+    *_remainder = numerator % denominator;
+    return 0;
+}
+void learn() {
+    /* 
+     int a = 10;
+    int b = a >> 1;
+    printf("%d",b);
+    printf("");
+    printf("%d",~b);
+    char arr[] = "abc";
+    arr[3] = 'd';
+    printf("%s", arr);
+    printf("%i", arr[11211]);
+    char arr[] = "abc";
+
+    for (int i = 0; i < 10; i++) {
+        printf("%p -> %d\n", (void *)&arr[i], arr[i]);
+    }
+
+    printf("%i",arr[-9999999999999999999]);
+    char d[10] = "heloddddd";
+    int instance_of_d = 0;
+
+    int len_d = strlen(d);
+    int j = 0;
+
+    while (j < len_d) {
+        if (d[j] == 'd') {
+            instance_of_d += 1;
+        }
+        j++;
+    };
+
+    printf("%i", instance_of_d);
+
+    char s[10],t[10];
+    int i,j;
+    strcpy(s,"frog");
+    for (i=0; i<strlen(s); i++)
+    t[i]=s[i];
+    j=0;
+    for (i=0; i<strlen(t); i++)
+    j=j+(int)t[i];
+    printf("%d\n",j);
+
+    char name[50],first[25],last[25];
+    printf("Enter a name : ");
+    scanf("%s", name);
+    int i = 1;
+    first[0] = name[0];
+    while (name[i] > 96 && name[i] < 123) {
+        first[i] = name[i];
+        i++;
+    }
+    first[i+1] = '\0';
+    while (i < strlen(name)) {
+        last[i] = name[i];
+        i++;
+    }
+    last[i+1] = '\0';
+    printf("%s", name);
+    printf("%s", first);
+    printf("%s", last);
+    int c, *cp;
+    c = 10;
+    cp = &c;
+    *cp = 1;
+    printf("%i", c);
+    printf("\n");
+    printf("%p\nff", cp);
+    int numerator = 100;
+    int denominator = 10;
+    int dividend, remainder;
+    division(numerator, denominator, &dividend, &remainder);
+    printf("%i\n", numerator);
+    printf("%i\n", denominator);
+    printf("%i\n", dividend);
+    printf("%i\n", remainder);
+
+    int n;
+    printf("Enter the number of character of name : ");
+    scanf("%i", &n);
+    char name[n];
+    printf("Enter the name : ");
+    scanf("%s", name);
+    printf("%s", name);
+
+    char name[100];
+    printf("Enter a name : ");
+    scanf("%s", name);
+    char* new_name = realloc(&name, sizeof(name) * 2);
+
+    int* numbers;
+    int k = integers(5, &numbers);
+    int j;
+    for (j = 0; j < 5; j++) {
+        printf("%i\n", numbers[j]);
+        printf("%p\n", &numbers[j]);
+    }
+
+    struct Teacher person;
+    person.year = 1980;
+    person.age = 46;
+    char name[10] = "saisakthi";
+    strcpy(person.name, name);
+    person.ppg = 10.2;
+
+    printf("%s", person.name);
+
+    FILE *fpt;
+    char text[800];
+    fpt = fopen("output.txt","r");
+    fscanf(fpt, "%s", text);
+    printf("%s", text);
+    
+    */
+}
+
+int integers(int list_size, int** list) {
+    int n;
+    int i;
+    *list = (int* ) malloc(list_size*sizeof(int));
+
+    for (i = 0; i < list_size; i++) {
+        (*list)[i] = i + 10;
+    }
+    return 0;
+}
+
+struct Teacher {
+    char name[10];
+    int age;
+    int year;
+    double ppg;
+};
+
+
+
 int main() {
-    char a = 253;
-    printf("%c",a);
+    
 }
